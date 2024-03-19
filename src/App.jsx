f4023d936faa311b79d672a57154c0489b3f54fd
vUh0nGqUaNg <vuhongquangit@gmail.com>
2024-03-20 01:10:46 +0700

    VHQuang | Done Main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Header from './componets/header/Header';
+import MainLayout from './layouts/MainLayout';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
-    <Header />
+    <Routes>
+      <Route path='/' element={<MainLayout />} >
+      </Route>
+    </Routes>
   )
 }
 
--- src/componets/header/Header.jsx
+++ src/componets/header/Header.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import { IoIosSearch } from "react-icons/io";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-      <div className="bg-blue-200 border-b border-gray-200 fixed w-full">
-        <header className="container max-w-screen-xl bg-blue-400 mx-auto flex justify-between">
-          <div className="flex items-center">
-            <div>
-              <img src='https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png' style={{ width: '50px', height: '40px' }}></img>
-            </div>
-            <div className="ml-2">
-              <input type="text" placeholder="Search..."/>
+      <div className=" border-b bg-white border-gray-200 fixed w-full h-14">
+        <header className="container max-w-screen-xl mx-auto flex justify-between items-center h-full">
+          <div className="flex justify-center items-center">
+            <Link to={'/'}>
+              <img src='https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png' style={{ width: '50px', height: '40px' }} alt="dev-logo"></img>
+            </Link>
+            <div className="ml-2 relative">
+              <input type="text" placeholder="Search..." style={{width: '420px', height: '40px'}}/>
+              <button className="absolute right-0 bottom-0 p-1.5 bg-white rounded-md hover:bg-blue-200" >
+                <IoIosSearch className="w-7 h-7"/>
+              </button>
             </div>
           </div>
           <div>
-            Login & sign up
+            <Link to={'/enter'} className="h-10 w-20 bg-white mr-2 rounded-md hover:bg-blue-100 hover:underline hover:text-blue-700">
+              Log in
+            </Link>
+            <Link to={'/signup'} className=" h-10 w-20 bg-white font-medium rounded-md border-solid border-blue-700 border text-blue-700 hover:bg-blue-700 hover:text-white hover:underline" >
+              Create account
+            </Link>
           </div>
         </header>
       </div>
